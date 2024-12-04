0a72062263666f847a070b2e8dd90f3f2aaefabd
Elizabeth <elizabethg2796@tuckahoeschools.org>
2024-12-04 13:32:32 +0000

    added more examples

--- Loop Examples/doWhileExample.js
+++ Loop Examples/doWhileExample.js
@@ -1,1 +1,23 @@
+let outputDiv = document.getElementById("output"); 
+let passwordInput = document.getElementById("passwordInput");
+let submitButton = document.getElementById("submitButton"); 
 
+const correctPassword = "MaxVerstappen"; 
+outputDiv.innerHTML += "<h2> Using a password prompt</h2>"; 
+
+submitButton.addEventListener("click", () =>{
+    let password;
+    let attempt = 0; 
+
+    do{
+        password = passwordInput.value;
+
+        if(password === correctPassword){
+            outputDiv.innerHTML += "Access Granted!<br>"; 
+            passwordInput.disabled = true; 
+            submitButton.disabled = true; 
+        }else{
+            outputDiv.innerHTML += `Incorrect`
+        }
+    }
+}); 
